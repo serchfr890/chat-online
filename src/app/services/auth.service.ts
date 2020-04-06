990954a7f2d2592efc2fc7ff28efc205d91c5bea
Sergio Flores Ramírez <sergio.flores@axity.com>
2020-04-06 18:53:20 -0500

    Parte 2

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private angularFireAuth: AngularFireAuth) { }
+  constructor(private angularFireAuth: AngularFireAuth,
+              private router: Router) { }
 
   login(email: string, password: string) {
       return new Promise(((resolve, reject) => {
           this.angularFireAuth.signInWithEmailAndPassword(email, password).then(responseLoginFirebase => resolve(responseLoginFirebase))
               .catch(throwable => reject(throwable));
       }));
+  }
+
+  logout() {
+      this.angularFireAuth.signOut().then(r => this.router.navigate(['/login']));
   }
 }
